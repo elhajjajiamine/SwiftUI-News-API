f16804e385e4314f025d65c0be1518ee63a81434
elhajjajiamine <aminselhajjaji@gmail.com>
2020-10-19 07:35:40 +0100

    Initial Commit

---
+++ SwiftUI  News API/SwiftUI__News_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI__News_APIApp.swift
+//  SwiftUI  News API
+//
+//  Created by elhajjaji on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI__News_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
